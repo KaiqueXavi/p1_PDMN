0e8e10eaea725dff87c070294589f16ecda9d27d
Thiago Chagras <thiago.chagras@fatec.sp.gov.br>
2025-04-11 22:34:08 -0300

    feat(p1): teste componente busca

--- p1-pdmn/src/App.jsx
+++ p1-pdmn/src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Busca from './components/Busca'
 
-function App() {
-  return (
-    <div>Hello P1</div>
-  )
+class App extends React.Component {
+
+  onBuscaRealizada = (termoDeBusca) => {
+    console.log(termoDeBusca)
+  }
+
+  //o método render é obrigatório para todos os componentes que herdam de React.Component
+  //ele deve retornar o que será exibido na tela
+
+
+  render() {
+    return (
+      <div
+        className='grid justify-content'>
+        <div className='col-12'>
+          <Busca
+            dica ='Digite o cep desejado'
+            onBuscaRealizada={this.onBuscaRealizada}/>
+        </div>
+      </div>
+    )
+  }
 }
-
 export default App
