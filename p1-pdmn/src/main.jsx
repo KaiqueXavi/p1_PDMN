926a15f4b95dcc754619627aa40c0867993e69db
Kaique.Xavier <Kaiquecavalcantexavier@gmail.com>
2025-04-18 13:10:55 -0300

    Alterações locais antes do pull

--- p1-pdmn/src/main.jsx
+++ p1-pdmn/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import ViaCep from 'react-via-cep';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
