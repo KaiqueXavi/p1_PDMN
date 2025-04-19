f888d5196e9c0b0d8a6fd8cb12248dd00d23d1bf
Kaique.Xavier <Kaiquecavalcantexavier@gmail.com>
2025-04-19 15:31:40 -0300

    feat(p1): integra com a api da viacep

--- p1-pdmn/src/App.jsx
+++ p1-pdmn/src/App.jsx
@@ -9,17 +9,43 @@ class App extends React.Component {
   api = null
 
   state = {
-    mensagemErro: null
+    mensagemErro: null,
+    cep: null,
+    logradouro: null,
+    bairro: null,
+    localidade: null,
+    uf: null
   }
 
-  onBuscaRealizada = (termoDeBusca) => {
-    if (termoDeBusca == ""){
-      this.setState({
-        mensagemErro: 'CEP inválido'
-      })
-      console.log("cep inválido")
+  onBuscaRealizada = async(termoDeBusca) => {
+
+    const cepPuro = termoDeBusca.replace(/\D/g, '')
+
+    if (cepPuro.length !== 8){
+        this.setState({
+          mensagemErro: 'CEP inválido'
+        })
+        console.log("CEP inválido")
+        return;
     }
-    console.log(termoDeBusca)
+    try{
+      const response = await api.get(`/${cepPuro}/json/`)
+      this.setState({
+        cep: termoDeBusca,
+        logradouro: response.data.logradouro,
+        bairro: response.data.bairro,
+        localidade: response.data.localidade,
+        uf: response.data.uf,
+        mensagemErro: null
+      })
+      console.log(response)
+    }
+    catch(error){
+      this.setState({
+        mensagemErro: 'Erro ao buscar CEP'
+      })
+      console.log('Erro' + error)
+    }
   }
 
   //o método render é obrigatório para todos os componentes que herdam de React.Component
